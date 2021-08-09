edc8bd314f8c71441dcb0947b97e461f24e7ef38
Robert Soriano <sorianorobertc@gmail.com>
2021-08-09 16:46:28 +0800

    update demo base url

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? `/${pkg.name}/` : './',
 })
