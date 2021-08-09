3d61df3f3cfa000af2ccb86780a10c0578418874
Robert Soriano <sorianorobertc@gmail.com>
2021-08-09 16:49:45 +0800

    update demo

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? `/${pkg.name}/` : './',
+  base: process.env.NODE_ENV === 'production' ? '/pinia-shared-state/' : './',
 })
