bfc9902b973f5857ebd4cd9c151a09d29056f933
Robert Soriano <sorianorobertc@gmail.com>
2022-03-09 14:57:57 -0800

    fix missing types

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === "production" ? "/pinia-shared-state/" : "./",
-});
+  base: process.env.NODE_ENV === 'production' ? '/pinia-shared-state/' : './',
+})
