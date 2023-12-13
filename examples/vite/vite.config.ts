a98cdbb8fb1d03a7d53d12dc373eea1bf4fa3cb2
wobsoriano <sorianorobertc@gmail.com>
2023-12-13 10:27:14 -0800

    eslint update

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // eslint-disable-next-line node/prefer-global/process
   base: process.env.NODE_ENV === 'production' ? '/pinia-shared-state/' : './',
 })
